65d6c587f9b8451eeaa8ec3f4f69e0abc048ee71
Narin <mukatov_n@iuca.kg>
2026-04-14 13:48:34 +0600

    Implement CRUD API for products using switch case

--- index.js
+++ index.js
@@ -1,67 +1,101 @@
 const http = require("http")
 const port = 3000
 
-const userArr = [
-    {id:1, name:"Alikhan"},
-    {id:2, name:"Nurislam"}
+const products = [
+    {id:1, name:"Laptop", price:999.99, category:"electronics"},
+    {id:2, name:"Book", price:19.99, category:"books"}
 ]
+
 const server = http.createServer((req,res) => {
     const {method, url} = req
     res.setHeader('Content-Type', 'application/json');
-    //GET ALL USER
-    if(method === "GET" && url ==="/users"){
-        res.writeHead(200)
-        res.end(JSON.stringify(userArr))
-    }
 
-    //GET ONE USER
-    else if (url.startsWith("/users/") && method =="GET"){
-        const id = parseInt(url.split("/")[2])
-        const findUser = userArr.find((user) => user.id === id) 
-        if(!findUser){
-            res.writeHead(404)
-            return res.end(JSON.stringify({message:"ERROR"}))
-        }
-        res.writeHead(200)
-        res.end(JSON.stringify(findUser))
-    }
+    switch(method) {
+        case 'GET':
+            if(url === '/products') {
+                res.writeHead(200)
+                res.end(JSON.stringify(products))
+            } else if (url.startsWith('/products/')) {
+                const id = parseInt(url.split('/')[2])
+                const product = products.find(p => p.id === id)
+                if(!product){
+                    res.writeHead(404)
+                    return res.end(JSON.stringify({message:"Product not found"}))
+                }
+                res.writeHead(200)
+                res.end(JSON.stringify(product))
+            } else {
+                res.writeHead(404)
+                res.end(JSON.stringify({message:"Not found"}))
+            }
+            break;
 
-    // CREATE NEW UESR
+        case 'POST':
+            if(url === '/products') {
+                let body = ''
+                req.on('data', chunk => {
+                    body += chunk.toString()
+                })
+                req.on('end', () => {
+                    const newProduct = JSON.parse(body)
+                    newProduct.id = products.length ? products[products.length-1].id + 1 : 1
+                    products.push(newProduct)
+                    res.writeHead(201)
+                    res.end(JSON.stringify(newProduct))
+                })
+            } else {
+                res.writeHead(404)
+                res.end(JSON.stringify({message:"Not found"}))
+            }
+            break;
 
-    else if(method === "POST" && url ==="/users/"){
-        let body = ''
-        req.on("data", chunck=> {
-            body += chunck.toString()
-        })
-        req.on("end", ()=> {
-            const newUser = JSON.parse(body)
-            newUser.id = userArr.length? userArr[userArr.length-1].id+1:1
-            userArr.push(newUser)
-            res.writeHead(201)
-            res.end(JSON.stringify(newUser))
-        })
-    }
+        case 'PUT':
+            if(url.startsWith('/products/')) {
+                const id = parseInt(url.split('/')[2])
+                const index = products.findIndex(p => p.id === id)
+                if(index === -1){
+                    res.writeHead(404)
+                    return res.end(JSON.stringify({message:"Product not found"}))
+                }
+                let body = ''
+                req.on('data', chunk => {
+                    body += chunk.toString()
+                })
+                req.on('end', () => {
+                    const updatedProduct = JSON.parse(body)
+                    products[index] = {...products[index], ...updatedProduct}
+                    res.writeHead(200)
+                    res.end(JSON.stringify(products[index]))
+                })
+            } else {
+                res.writeHead(404)
+                res.end(JSON.stringify({message:"Not found"}))
+            }
+            break;
 
-    else if(method == "PUT" && url.startsWith("/users/")){
-        const id = parseInt(url.split("/")[2])
-        const index = userArr.findIndex((user) => user.id === id) 
-        if(index <= -1){
-            res.writeHead(404)
-            return res.end(JSON.stringify({message:"ERROR"}))
-        }
-        let body = ''
-        req.on("data", chunck=> {
-            body += chunck.toString()
-        })
-        req.on("end", ()=> {
-            const updatedUser = JSON.parse(body)
-            userArr[index] = {...userArr[index], ...updatedUser}
-            res.writeHead(201)
-            res.end(JSON.stringify(updatedUser))
-        })
+        case 'DELETE':
+            if(url.startsWith('/products/')) {
+                const id = parseInt(url.split('/')[2])
+                const index = products.findIndex(p => p.id === id)
+                if(index === -1){
+                    res.writeHead(404)
+                    return res.end(JSON.stringify({message:"Product not found"}))
+                }
+                products.splice(index, 1)
+                res.writeHead(200)
+                res.end(JSON.stringify({message:"Product deleted"}))
+            } else {
+                res.writeHead(404)
+                res.end(JSON.stringify({message:"Not found"}))
+            }
+            break;
+
+        default:
+            res.writeHead(405)
+            res.end(JSON.stringify({message:"Method not allowed"}))
     }
 })
 
 server.listen(port, () =>{
-    console.log(`sever work on http://localhost:${port} `)
+    console.log(`Server running on http://localhost:${port}`)
 })
